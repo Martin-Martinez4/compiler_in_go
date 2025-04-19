8fa963a1ca4cbb4dad19d905b62598086b9abd63
Martin-Martinez-4 <martmartez4@gmail.com>
2025-04-18 22:16:51 -0700

    Started compiler and bytecode gen

---
+++ compiler/compiler.go
@@ -0,0 +1,36 @@
+package compiler
+
+import (
+	"github.com/Martin-Martinez4/compiler_in_go/ast"
+
+	"github.com/Martin-Martinez4/compiler_in_go/code"
+	"github.com/Martin-Martinez4/compiler_in_go/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
