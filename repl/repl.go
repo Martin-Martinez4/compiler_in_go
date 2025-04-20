207d25dfb9a82410764ac51f281c600111314e38
Martin-Martinez-4 <martmartez4@gmail.com>
2025-04-19 20:44:23 -0700

    Got to chapter 4 in the book; added simple arithmetic operations, booleans, and simple comparisons

--- repl/repl.go
+++ repl/repl.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/Martin-Martinez4/compiler_in_go/evaluator"
+	"github.com/Martin-Martinez4/compiler_in_go/compiler"
 	"github.com/Martin-Martinez4/compiler_in_go/lexer"
-	"github.com/Martin-Martinez4/compiler_in_go/object"
 	"github.com/Martin-Martinez4/compiler_in_go/parser"
+	"github.com/Martin-Martinez4/compiler_in_go/vm"
 )
 
 const PROMPT = "-> "
@@ -22,7 +22,7 @@ func printParseErrors(out io.Writer, errors []string) {
 }
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
+	// env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -41,12 +41,31 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
+		// evaluated := evaluator.Eval(program, env)
+		// if evaluated != nil {
 
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		// 	io.WriteString(out, evaluated.Inspect())
+		// 	io.WriteString(out, "\n")
+		// }
+
+		// hook up to compiler
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		lastPopped := machine.LastPoppedStackElem()
+		io.WriteString(out, lastPopped.Inspect())
+		io.WriteString(out, "\n")
 
 	}
 }
---
+++ vm/vm_test.go
@@ -0,0 +1,135 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Martin-Martinez4/compiler_in_go/ast"
+	"github.com/Martin-Martinez4/compiler_in_go/compiler"
+	"github.com/Martin-Martinez4/compiler_in_go/lexer"
+	"github.com/Martin-Martinez4/compiler_in_go/object"
+	"github.com/Martin-Martinez4/compiler_in_go/parser"
+)
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntergetObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer.  got=%T (%v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value.  got=%d, want=%d", result.Value, expected)
+	}
+
+	return nil
+}
+
+func testBooleanObject(expected bool, actual object.Object) error {
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean.  got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value.  got=%T, want=%t", result.Value, expected)
+	}
+
+	return nil
+}
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.LastPoppedStackElem()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntergetObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+
+	case bool:
+		err := testBooleanObject(bool(expected), actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err)
+		}
+	}
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 3},
+		{"1 - 2", -1},
+		{"2 * 2", 4},
+		{"6/2", 3},
+		{"50 / 2 * 2 + 10 - 5", 55},
+		{"5 + 5 + 5 + 5 - 5", 15},
+		{"5 * (2 + 10)", 60},
+		{"5 * 2 + 10", 20},
+		{"-5", -5},
+		{"-10", -10},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+	}
+
+	runVmTests(t, tests)
+
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+		{"1 < 2", true},
+		{"1 == 1", true},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 > 2) == false", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"!true", false},
+		{"!false", true},
+		{"!!true", true},
+		{"!!false", false},
+		{"!5", false},
+		{"!!5", true},
+	}
+
+	runVmTests(t, tests)
+}
